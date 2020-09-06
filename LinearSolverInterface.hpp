c1bdf7cf8aa0318e003e0f4616c88b851ad08d89
Sibusiso Mabuza <ssmabuza@outlook.com>
2020-09-06 00:20:06 -0400

    Added a linear solver interface

---
+++ LinearSolverInterface.hpp
@@ -0,0 +1,34 @@
+
+#ifndef __Math8650_LinearSolverInterface_HPP__
+#define __Math8650_LinearSolverInterface_HPP__
+
+#include "DenseMatrix.hpp"
+
+namespace math8650
+{
+
+class LinearSolverInterface
+{
+  
+  static const int maxiter = 10000;
+  constexpr static double tol = 1.0e-06;
+  constexpr static double eps = 1.0e-15;
+
+public:
+
+  //! \brief method solves a linear system using the Jacobi preconditioned CG 
+  static void solveSystemCG(const std::shared_ptr<const DenseMatrix>& A, 
+                            const std::shared_ptr<const DenseVector>& rhs, 
+                            const std::shared_ptr<DenseVector>& sol);
+
+  //! \brief method solves a linear system using the Jacobi preconditioned BiCGStab 
+  static void solveSystemBiCGStab(const std::shared_ptr<const DenseMatrix>& A, 
+                                  const std::shared_ptr<const DenseVector>& rhs, 
+                                  const std::shared_ptr<DenseVector>& sol);
+
+};
+
+}
+// end namespace math8650
+
+#endif /** __Math8650_LinearSolverInterface_HPP__ */
